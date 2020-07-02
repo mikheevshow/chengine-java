dd81004539776026314252657f20b5ee92853a13
ilyamikheev <mikheev.show@gmail.com>
2020-07-02 15:29:25 +0300

    fix doc

--- core/src/main/java/io/chengine/annotation/CommandDescription.java
+++ core/src/main/java/io/chengine/annotation/CommandDescription.java
@@ -3,16 +3,20 @@ package io.chengine.annotation;
 import java.lang.annotation.*;
 
 /**
- * Annotation is used to add a description to @CommandHandler
+ * Annotation is used to add a description to {@link HandleCommand}
  * At this time, the description must match the specified format.
  * Further development is planned due to the addition of locale
- *
- *
- * Example of adding a description
- * Over the method to add a description for, you need to annotate @CommandDescription(
- * description = "{RU-ru : Method description}, {EN-en : Method description}");
+ * <p>
+ * Example of adding a description:
+ * Over the method to add a description for, you need to annotate
+ * <pre>
+ *      @CommandDescription(description = "{RU-ru : Method description}, {EN-en : Method description}");
+ * </pre>
  * After that, when calling the /help command, the BOT will describe this command
  * as specified in the description, taking into account the localization.
+ *
+ * @author Andrey Borisov
+ *
  */
 @Documented
 @Target(ElementType.METHOD)
@@ -20,4 +24,5 @@ import java.lang.annotation.*;
 public @interface CommandDescription {
 
     String[] description() default "";
+
 }
