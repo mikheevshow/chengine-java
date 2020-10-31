f78892a0f35a83bac732c848dd54bb51f50c4712
Mikheev <mikheev.show@gmail.com>
2020-10-31 15:10:46 +0300

    muted vk

--- vk/src/main/java/io/chengine/VkLongPoolingBot.java
+++ vk/src/main/java/io/chengine/VkLongPoolingBot.java
@@ -1,34 +1,34 @@
-package io.chengine;
-
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-public class VkLongPoolingBot {
-
-    private final HttpTransportClient client = new HttpTransportClient();
-    private final VkApiClient apiClient = new VkApiClient(client);
-    private final GroupActor groupActor;
-
-    public VkLongPoolingBot(
-            final int groupId,
-            final String token,
-            final int serverId,
-            final String code) {
-
-        groupActor = new GroupActor(groupId, token);
-        try {
-            apiClient
-                    .groups()
-                    .setCallbackSettings(groupActor, serverId)
-                    .messageNew(true)
-                    .execute();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-
-    }
-
-}
+//package io.chengine;
+//
+//import com.vk.api.sdk.client.VkApiClient;
+//import com.vk.api.sdk.client.actors.GroupActor;
+//import com.vk.api.sdk.httpclient.HttpTransportClient;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//public class VkLongPoolingBot {
+//
+//    private final HttpTransportClient client = new HttpTransportClient();
+//    private final VkApiClient apiClient = new VkApiClient(client);
+//    private final GroupActor groupActor;
+//
+//    public VkLongPoolingBot(
+//            final int groupId,
+//            final String token,
+//            final int serverId,
+//            final String code) {
+//
+//        groupActor = new GroupActor(groupId, token);
+//        try {
+//            apiClient
+//                    .groups()
+//                    .setCallbackSettings(groupActor, serverId)
+//                    .messageNew(true)
+//                    .execute();
+//        } catch (Exception e) {
+//            log.error(e.getMessage(), e);
+//        }
+//
+//    }
+//
+//}
