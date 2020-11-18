da52fb049c0f81c1061c7288adf0ea1ab0667a4a
ilyamikheev <mikheev.show@gmail.com>
2020-11-18 21:49:56 +0300

    fix chengine class

--- core/src/main/java/io/chengine/Chengine.java
+++ core/src/main/java/io/chengine/Chengine.java
@@ -4,10 +4,14 @@ import io.chengine.connector.BotRequest;
 import io.chengine.connector.BotResponse;
 import io.chengine.context.ChengineHandlerContext;
 import io.chengine.method.MethodArgumentInspector;
+import io.chengine.pipeline.processor.DefaultPipelineRequestHandler;
 import io.chengine.pipeline.processor.PipelineRequestHandler;
 import io.chengine.processor.*;
+import io.chengine.session.ChengineSessionContext;
+import io.chengine.session.SessionCache;
 import io.chengine.session.SessionManager;
 import io.chengine.session.UserPipelineSessionInfo;
+import io.chengine.session.pipeline.PipelineSessionManager;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -15,28 +19,42 @@ import java.util.List;
 // TODO
 public class Chengine {
 
-//    private MethodResolver commandMethodResolver = CommandMethodResolver(); // <- перенесено (инициализировать)
-//    private SessionManager<UserPipelineSessionInfo> userPipelineSessionInfoSessionManager;
-//    private PipelineRequestHandler pipelineRequestHandler;
-//    private MessageResolverFactory messageResolverFactory; // <- перенесено (инициализировать)
-//    private MethodReturnedTypeProcessor methodReturnedTypeProcessor;
-//    private MethodArgumentInspector methodArgumentInspector; // <- перенесено (инициализировать)
-//    private MessageProcessor<BotRequest, BotResponse> messageProcessor;
-//    private HandlerRegistry handlerRegistry;
-//    private List<RequestHandler> botList;
+    private CommandMethodResolver commandMethodResolver; // <- перенесено (инициализировать)
+    private SessionManager<UserPipelineSessionInfo> userPipelineSessionInfoSessionManager;
+    private SessionCache<UserPipelineSessionInfo> sessionCache;
+    private PipelineRequestHandler pipelineRequestHandler;
+    private MessageResolverFactory messageResolverFactory; // <- перенесено (инициализировать)
+    private MethodReturnedTypeProcessor methodReturnedTypeProcessor;
+    private MethodArgumentInspector methodArgumentInspector; // <- перенесено (инициализировать)
+    private MessageProcessor<BotRequest, BotResponse> messageProcessor;
+    private HandlerRegistry handlerRegistry;
+    private List<RequestHandler> botList;
 
-    private Chengine() {}
+    private Chengine() {
+    }
 
     public Chengine(ChengineConfiguration configuration) {
-
-//        this.handlerRegistry = new ChengineHandlerContext(configuration);
-
+        this.handlerRegistry = new ChengineHandlerContext(configuration);
     }
 
     @PostConstruct
     private void init() {
-        initMethodResolvers();
-        setMessageProcessorForBots();
+        this.commandMethodResolver = new CommandMethodResolver(this.handlerRegistry);
+        this.sessionCache = new ChengineSessionContext<>();
+        this.userPipelineSessionInfoSessionManager = new PipelineSessionManager(this.sessionCache);
+        this.pipelineRequestHandler = new DefaultPipelineRequestHandler();
+        this.messageResolverFactory = new MessageResolverFactory(this.commandMethodResolver);
+        this.methodReturnedTypeProcessor = new MethodReturnedTypeProcessor();
+        this.methodArgumentInspector = new MethodArgumentInspector();
+        this.messageProcessor = new ChengineMessageProcessor(
+                this.commandMethodResolver,
+                this.methodArgumentInspector,
+                this.methodReturnedTypeProcessor,
+                this.userPipelineSessionInfoSessionManager,
+                this.pipelineRequestHandler
+        );
+
+        this.botList.forEach(bot -> bot.setMessageProcessor(messageProcessor));
     }
 
     private void initMethodResolvers() {
